059ea00eb60be001ed8b135d1f2554690cd38121
Dang Hoang Minh <43031977+danghoangminh@users.noreply.github.com>
2021-07-06 16:36:56 +0700

    Update ucBooks.Designer.cs

--- BookStoreManagement/View/UserControls/ucBooks.Designer.cs
+++ BookStoreManagement/View/UserControls/ucBooks.Designer.cs
@@ -118,7 +118,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(532, 27);
+            this.label5.Location = new System.Drawing.Point(523, 26);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(96, 20);
             this.label5.TabIndex = 4;
